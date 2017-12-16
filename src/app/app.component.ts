37c734db084ea62850b45d207e9235200d7b24e0
Jimmy <jiaming0708@gmail.com>
2017-12-16 11:28:36 +0800

    add data on init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,17 @@
 import { Component } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
+import { OnInit } from '@angular/core/src/metadata/lifecycle_hooks';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styles: []
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   formData = this.fb.group({
-    'firstName': ['jimmy', Validators.required],
-    'lastName': [{ value: 'ho', disabled: true }, Validators.required],
-    'phone': ['', [Validators.required, Validators.minLength(8)]]
+    'firstName': [, Validators.required],
+    'lastName': [, Validators.required],
+    'phone': [, [Validators.required, Validators.minLength(8)]]
   });
   // formData = new FormGroup({
   //   'firstName': new FormControl('jimmy', Validators.required),
@@ -21,6 +22,12 @@ export class AppComponent {
     // console.log(form);
     console.log('value', this.formData.value);
     console.log('rawValue', this.formData.getRawValue());
+  }
+
+  ngOnInit() {
+    this.formData.reset({
+      'firstName': 'jimmy'
+    })
   }
 
   constructor(private fb:FormBuilder) {}
